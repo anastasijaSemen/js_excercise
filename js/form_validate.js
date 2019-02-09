6f4e5922f681998ea5b8b541183dee5c4cfb5641
Anastasija Semen <anastasija.semen@gmail.com>
2019-02-09 15:25:36 +0200

    implementation reset button

---
+++ js/form_validate.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+document.getElementById('reset').addEventListener('click', function(e){
+ document.getElementById('name').value = "";
+ document.getElementById('adress1').value = "";
+ document.getElementById('adress2').value = "";
+ document.getElementById('city').value = "";
+ document.getElementById('state').value = "";
+ document.getElementById('zipcode').value = "";
+});
